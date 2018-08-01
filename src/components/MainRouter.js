f5b01afd18ec6207c31a05893ae0eabd11a1a014
Sam Lau <esayemm@gmail.com>
2018-08-01 01:03:42 -0700

    add flowtype

--- src/components/MainRouter.js
+++ src/components/MainRouter.js
@@ -1,10 +1,11 @@
+// @flow
 import { Route }         from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 
 export default function MainRouter() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={() => <div>Hello World</div>} />
+      <Route path="/" exact component={() => <div>Hello world!</div>} />
     </BrowserRouter>
   )
 }
